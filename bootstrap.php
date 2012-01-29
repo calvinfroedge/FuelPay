204d785461b6e62982c4dbe1e7273c8fb0db8f13
Calvin Froedge <calvinfroedge@gmail.com>
2012-01-29 10:23:19 -0600

    Updated package name to 'FuelPay'

--- bootstrap.php
+++ bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 // Add as core namespace
-Autoloader::add_core_namespace('Payment');
+Autoloader::add_core_namespace('FuelPay');
 
 // And add the classes, this is usefull for:
 // - optimization: no path searching is necessary
 // - it's required to be able to use as a core namespace
 // - if you want to break the autoloader's path search rules
 Autoloader::add_classes(array(
-    'Payment\\Payment' => __DIR__.'/classes/payment.php'
+    'FuelPay\\FuelPay' => __DIR__.'/classes/FuelPay.php'
 ));
