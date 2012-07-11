2eb43442615e4d640b5b60d46d581a308c25f2f1
huglester <huglester@gmail.com>
2012-07-11 11:30:50 +0300

    Update master

--- bootstrap.php
+++ bootstrap.php
@@ -7,5 +7,5 @@ Autoloader::add_core_namespace('FuelPay');
 // - it's required to be able to use as a core namespace
 // - if you want to break the autoloader's path search rules
 Autoloader::add_classes(array(
-    'FuelPay\\FuelPay' => __DIR__.'/classes/FuelPay.php'
+    'FuelPay\\FuelPay' => __DIR__.'/classes/payment.php'
 ));
